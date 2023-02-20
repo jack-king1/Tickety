12801eaa035345bb8ccfd1c69b48d7bf13c3b1d8
jack-king1 <jackk@neonplay.com>
2023-02-20 02:20:46 +0000

    init bootstrap + adding navbar

---
+++ ecom-site/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Products from "./Components/Products";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <div className="">
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ ecom-site/src/Components/Navbar.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+import "popper.js/dist/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <Link className="navbar-brand " to="/">
+          <h1 className="text-xl text-light">Tickety</h1>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                to="/products"
+              >
+                Products
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                to="/about"
+              >
+                About Us
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                to="/contact"
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    //   <div className="container">
+    //     <div className="d-flex justify-content-between w-100">
+    //       <Link className="navbar-brand mr-auto" to="/">
+    //         Ecommerce Site
+    //       </Link>
+    //       <button
+    //         className="navbar-toggler"
+    //         type="button"
+    //         data-toggle="collapse"
+    //         data-target="#navbarNav"
+    //         aria-controls="navbarNav"
+    //         aria-expanded="false"
+    //         aria-label="Toggle navigation"
+    //       >
+    //         <span className="navbar-toggler-icon"></span>
+    //       </button>
+    //       <div className="collapse navbar-collapse" id="navbarNav">
+    //         <ul className="navbar-nav">
+    //           <li className="nav-item">
+    //             <NavLink className="nav-link" to="/products">
+    //               Products
+    //             </NavLink>
+    //           </li>
+    //           <li className="nav-item">
+    //             <NavLink className="nav-link" to="/about">
+    //               About Us
+    //             </NavLink>
+    //           </li>
+    //           <li className="nav-item">
+    //             <NavLink className="nav-link" to="/contact">
+    //               Contact Us
+    //             </NavLink>
+    //           </li>
+    //         </ul>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </nav>
+  );
+}
+
+export default Navbar;
