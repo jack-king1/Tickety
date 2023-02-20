c6e724d0916e4e7d09de315cf19a5394f37f871c
jack-king1 <jackk@neonplay.com>
2023-02-20 18:44:46 +0000

    started working on grid home page

--- ecom-site/src/Components/Home.js
+++ ecom-site/src/Components/Home.js
@@ -1,7 +1,52 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="well">
+            1
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="well">2</div>
+            </div>
+            <div class="col-md-6">
+              <div class="well">3</div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="well">4</div>
+            </div>
+            <div class="col-md-6">
+              <div class="well">5</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="well">6</div>
+        </div>
+        <div class="col-md-4">
+          <div class="well">7</div>
+        </div>
+        <div class="col-md-4">
+          <div class="well">8</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
