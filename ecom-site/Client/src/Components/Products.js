de930f760d17c4f2cae5d8a2696a9fabb7df56d5
jack-king1 <jackking.gm97@gmail.com>
2023-02-28 19:00:22 +0000

    removed debug click button

--- ecom-site/Client/src/Components/Products.js
+++ ecom-site/Client/src/Components/Products.js
@@ -4,6 +4,10 @@ import Axios from "axios";
 function Products() {
   const [productList, setProductList] = useState([]);
 
+  useEffect(() => {
+    debugCLick();
+  }, []);
+
   const debugCLick = () => {
     Axios.get("http://localhost:3001/api/get").then((response) => {
       console.log(response);
@@ -14,7 +18,6 @@ function Products() {
   return (
     <div className="container">
       <div>
-        <button onClick={debugCLick}>CLick Me</button>
         {productList.map((val) => {
           return (
             <div className="border border-primary container mt-3">
@@ -22,10 +25,15 @@ function Products() {
                 <div id="previewImg" className="col-2">
                   IMG
                 </div>
-                <div id="productInfo" className="col-10">
+                <div id="productInfo" className="col-7">
                   <h1>{val.productName}</h1>
                   <h4>{val.productDesc}</h4>
-                  <h3>£{val.productPrice}</h3>
+                  <h3 className="font-weight-bold">£{val.productPrice}</h3>
+                </div>
+                <div className="col-3">
+                  <button type="button" class="btn btn-success">
+                    Add To Cart
+                  </button>
                 </div>
               </div>
             </div>
